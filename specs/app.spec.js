05d246ac09ebb0ca64575fe740ebdf66133e6c3d
Yura <yukokanin@chr.rsvo.ru>
2023-03-01 11:18:59 +0300

    Changed name and trim tests to test.each

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -1,5 +1,3 @@
-import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
-
 /**
  * Для проверки, что jest настроен правильно. Можно удалить
  */
@@ -7,43 +5,30 @@ test('adds 1 + 2 to equal 3', () => {
     expect(1 + 2).toBe(3);
 });
 
-const name0 = [["a","23y78",123],true];
-const name1 = [1,false];
-const name2 = ["p 0",false];
-const name3 = ["hasd!@das.ru",true];
-const string0 = ["",""];
-const string1 = ["843 243875 ","843243875"];
-const string2 = [" Для проверки, что jest настроен правильно. Можно удалить","Дляпроверки,чтоjestнастроенправильно.Можноудалить"];
-const string3 = ["   ",""];
+import { nameIsValid, fullTrim, getTotal } from '../src/app.js'
 
-describe('test name', () => {
-test(`isNameValid > given name: ${name0[0]} | expected result: ${name0[1]}`, () => {
-    expect(nameIsValid(name0[0])).toBe(name0[1])
-});
-test(`isNameValid > given name: ${name1[0]} | expected result: ${name1[1]}`, () => {
-    expect(nameIsValid(name1[0])).toBe(name1[1])
-});
-test(`isNameValid > given name: ${name2[0]} | expected result: ${name2[1]}`, () => {
-    expect(nameIsValid(name2[0])).toBe(name2[1])
-});
-test(`isNameValid > given name: ${name3[0]} | expected result: ${name3[1]}`, () => {
-    expect(nameIsValid(name3[0])).toBe(name3[1])
-});
+describe('test function nameIsValid', () => {
+  const data = [
+    { name: ["a","23y78",123], isValid: true },
+    { name: 1, isValid: false },
+    { name: "p 0", isValid: false },
+    { name: "hasd!@das.ru", isValid: true },
+  ];
+  test.each(data)('isNameValid > given name: $name | expected result: $isValid', ({ name, isValid }) => {
+    expect(nameIsValid(name)).toBe(isValid)
+  })
 });
 
-describe('trim spaces', () => {
-test(`fullTrim > given string: ${string0[0]} | expected result: ${string0[1]}`, () => {
-    expect(fullTrim(string0[0])).toBe(string0[1])
-});
-test(`fullTrim > given string: ${string1[0]} | expected result: ${string1[1]}`, () => {
-    expect(fullTrim(string1[0])).toBe(string1[1])
-});
-test(`fullTrim > given string: ${string2[0]} | expected result: ${string2[1]}`, () => {
-    expect(fullTrim(string2[0])).toBe(string2[1])
-});
-test(`fullTrim > given string: ${string3[0]} | expected result: ${string3[1]}`, () => {
-    expect(fullTrim(string3[0])).toBe(string3[1])
-});
+describe('test function fullTrim', () => {
+  const data = [
+    { string: "", expected: "" },
+    { string: "843 243875 ", expected: "843243875" },
+    { string: " Для проверки, что jest настроен правильно. Можно удалить", expected: "Дляпроверки,чтоjestнастроенправильно.Можноудалить" },
+    { string: "   ", expected: "" },
+  ];
+  test.each(data)('isStringTrimmed > given string: $string | expected result: $expected', ({ string, expected }) => {
+    expect(fullTrim(string)).toBe(expected)
+  })
 });
 
 describe.each([{x: 1, y: 1, z: 0, expected: 1, extra: 'sine'}, {x: 100, y: 100, z: 10, expected: 9000, extra: 0}, {x: 10, y: 10, z: 100, expected: 0, extra: false}])(
